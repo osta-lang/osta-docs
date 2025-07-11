f835fb9309efc084e30478ece5b098042bfa0196
JohanVonElectrum <joanestradium@gmail.com>
2025-07-11 09:09:24 +0200

    Fix Get Started link path on homepage
    Fixed the 'Get Started' button link to point to '/docs/guides/getting-started' instead of '/docs/getting-started'.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
                 <div className={styles.buttons}>
                     <Link
                         className="button button--secondary button--lg"
-                        to="/docs/getting-started">
+                        to="/docs/guides/getting-started">
                         Get Started
                     </Link>
                 </div>
